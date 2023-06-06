5359ccbd21b6315a5ce2f3c35039695aec862346
Valerii Rublev <vedius.rublev@gmail.com>
2023-06-06 17:29:42 +0300

    parse

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,47 @@
+import csv
+from datetime import datetime
+from pathlib import Path
+
+from pep_parse.items import PepParseItem
+
+BASE_DIR = Path(__file__).absolute().parent.parent
+DATETIME_FORMAT = "%Y-%m-%d_%H-%M-%S"
+
+
+class PepParsePipeline:
+    def __init__(self):
+        self.pep_statuses = {}
+
+    def open_spider(self,spider):
+        pass
+
+    def process_item(self, item: PepParseItem, spider):
+        pep_status = item["status"]
+        if pep_status not in self.pep_statuses.keys():
+            self.pep_statuses[pep_status] = 1
+        else:
+            self.pep_statuses[pep_status] += 1
+
+        return item
+
+    def close_spider(
+        self,
+        spider,
+    ):
+        results = [("Статус", "Количество")]
+        for keys, values in self.pep_statuses.items():
+            results.append((keys, values))
+        results.append(
+            ("Total", sum(self.pep_statuses.values())),
+        )
+
+        results_dir = BASE_DIR / "results"
+        results_dir.mkdir(exist_ok=True)
+
+        now = datetime.now()
+        now_formatted = now.strftime(DATETIME_FORMAT)
+        file_name = f"status_summary_{now_formatted}.csv"
+        file_path = results_dir / file_name
+        with open(file_path, "w", encoding="utf-8") as f:
+            writer = csv.writer(f, dialect="unix")
+            writer.writerows(results)
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,28 @@
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = "pep"
+    allowed_domains = ["peps.python.org"]
+    start_urls = ["https://peps.python.org/"]
+
+    def parse(self, response):
+        for table in response.css("table"):
+            all_peps = table.css("td:nth-child(2) > a::attr(href)")
+            for pep_link in all_peps:
+                yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response: scrapy.http.Response):
+        pep_number, pep_name = response.css("h1.page-title::text").get().split(" – ")
+        pep_number = pep_number.replace("PEP ", "")
+        data = {
+            "number": pep_number,
+            "name": pep_name,
+            "status": response.css("dt:contains('Status') + dd ::text").get(),
+        }
+        # pep-content > dl > dd:nth-child(4) > abbr
+        # pep-content > dl > dd:nth-child(6) > abbr
+        print(data)
+        yield PepParseItem(data)
